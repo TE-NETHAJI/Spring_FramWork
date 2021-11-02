7443d19c0e557f000ea60eb6a3b633282c5d7dcd
Nethaji <subas1997@gmail.com>
2021-11-02 21:48:47 +0530

    Springcorewithxml

---
+++ springcore/src/main/java/com/te/springcore/beans/Employee.java
@@ -0,0 +1,23 @@
+package com.te.springcore.beans;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee implements Serializable {
+	
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String role;
+	
+	private DeptClass dept;
+	
+
+}
