61ded38dd6e235a8955a1311c151b71b6d3a535d
Nethaji <subas1997@gmail.com>
2021-11-03 15:21:35 +0530

    springcorewithannotations

--- springcore/src/main/java/com/te/springcore/AutoWireTest.java
+++ springcore/src/main/java/com/te/springcore/AutoWireTest.java
@@ -10,12 +10,13 @@ public class AutoWireTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("allConfig.xml");
 		
 		Employee emp = context.getBean("emp", Employee.class);
+		
 		System.out.println("EMP details");
 		System.out.println(emp.getName());
 		System.out.println(emp.getRole());
 		System.out.println("Department details");
 		System.out.println(emp.getDept().getDeptName());
-		System.out.println();
+		System.out.println(emp.getDept().getDeptId());
 	}
 
 }
--- springcore/src/main/java/com/te/springcore/beans/Employee.java
+++ springcore/src/main/java/com/te/springcore/beans/Employee.java
@@ -5,19 +5,24 @@ import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee implements Serializable {
-	
-	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private Integer id;
-	
+
 	private String name;
-	
+
 	private String role;
-	
+
 	private DeptClass dept;
-	
 
 }
